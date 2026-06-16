3fad48916c7c1964ddf6e8497f5a3525672fa8b1
OkanClaw Agent <okanclaw@okanclaw.dev>
2026-06-16 13:20:46 +0300

    fix: update Kotlin to 2.2.0, add NDK 27

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.morepara.fog_of_war"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
